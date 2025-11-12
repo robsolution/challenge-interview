b6cddce58d360e8220b027e36f9499a1db81f0e1
Robson Luis da Silva <robson.easyway@gmail.com>
2025-11-12 06:56:44 -0300

    ignore aws-iam-no-policy-wildcards on tfsec

--- terraform/tf_09_iam.tf
+++ terraform/tf_09_iam.tf
@@ -91,6 +91,7 @@ resource "aws_iam_policy" "vpc_builder_policy" {
         Effect   = "Allow"
         Resource = aws_dynamodb_table.vpc_requests.arn
       },
+      # tfsec:ignore:aws-iam-no-policy-wildcards
       {
         # EC2 permissions to create the VPC and its components.
         Action = [
--- terraform/tf_12_cloudwatch.tf
+++ terraform/tf_12_cloudwatch.tf
@@ -1,14 +1,17 @@
 resource "aws_cloudwatch_log_group" "api_gateway_logs" {
   name              = "/aws/apigateway/${var.project_name}-api-logs"
+  kms_key_id        = aws_kms_key.shared_key.arn
   retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "api_handler_logs" {
   name              = "/aws/lambda/${var.project_name}-ApiHandler"
+  kms_key_id        = aws_kms_key.shared_key.arn
   retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "vpc_builder_logs" {
   name              = "/aws/lambda/${var.project_name}-VpcBuilder"
+  kms_key_id        = aws_kms_key.shared_key.arn
   retention_in_days = 7
 }
